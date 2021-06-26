b1539501441e124318658b626ab0f770a2304b4b
Gubina Irina <irina.gubina98@mail.ru>
2021-06-26 19:24:32 +0300

    Исправлены css селектора

--- src/main/java/Avito.java
+++ src/main/java/Avito.java
@@ -11,8 +11,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Avito {
 
-    public static void findAndClick(WebDriver driver, String xPath) {
-        WebElement deliveryButton = driver.findElement(By.xpath(xPath));
+    public static void findAndClick(WebDriver driver, String cssSelector) {
+        WebElement deliveryButton = driver.findElement(By.cssSelector(cssSelector));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView();", deliveryButton);
         deliveryButton.click();
@@ -22,43 +22,44 @@ public class Avito {
         WebElement item = driver.findElement(By.cssSelector(cssSelector));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView();", item);
-        List<WebElement> printList = driver.findElements(By.cssSelector(".items-items-38oUm"));
-        for (int i = 0; i < printList.size(); i++) {
-            System.out.println("Название принтера: " +
-                    printList.get(i).findElement(By.cssSelector(".iva-item-titleStep-2bjuh")).getText()
-                    + " Стоимость принтера: " + printList.get(i).findElement(By.cssSelector(".iva-item-priceStep-2qRpg"))
-                    .getText());
-        }
     }
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 20);
 
         driver.get("https://www.avito.ru/");
         Select category = new Select(driver.findElement(By.id("category")));
         category.selectByValue("99");
-        driver.findElement(By.cssSelector(".suggest-root-1v2AH.js-suggest.suggest_search"))
+        driver.findElement(By.cssSelector("[data-marker='search-form/suggest']"))
                 .sendKeys("Принтер");
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".form-suggest-match"))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".main-text-2PaZG"))).click();
-        WebElement fieldCity = driver.findElement(By.cssSelector(".suggest-input-3p8yi"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector
+                ("[data-marker='suggest/list/item-with-category']"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[data-marker='search-form/region']")))
+                .click();
+        WebElement fieldCity = driver.findElement(By.cssSelector("[data-marker='popup-location/region/input']"));
         fieldCity.clear();
         fieldCity.sendKeys("Владивосток", Keys.ENTER);
-        driver.findElement(By.cssSelector(".suggest-suggest-content-KQ__w")).click();
-        driver.findElement(By.cssSelector(".button-button-2Fo5k.button-size-m-7jtw4.button-primary-1RhOG")).click();
-        findAndClick(driver, "/html/body/div[1]/div[3]/div[3]/div[1]/div/div[2]/div[1]/form/div[6]/div/" +
-                "div/div/div/div/div/label");
-        driver.findElement(By.cssSelector(".button-button-2Fo5k.button-size-s-3-rn6.button-primary-1RhOG." +
-                "width-width-12-2VZLz")).click();
+        driver.findElement(By.cssSelector("[data-marker='suggest(0)']")).click();
+        driver.findElement(By.cssSelector("[data-marker='popup-location/save-button']")).click();
+        findAndClick(driver, "[data-marker='delivery-filter/text']");
+        driver.findElement(By.cssSelector("[data-marker='search-filters/submit-button']")).click();
         WebElement option = driver.findElement(By.cssSelector(".sort-select-3QxXG.select-select-box-3LBfK" +
                 ".select-size-s-2gvAy"));
         option.click();
         Select moreExpensive = new Select(option.findElement(By.cssSelector(".select-select-3CHiM")));
         moreExpensive.selectByVisibleText("Дороже");
-        findItem(driver, ".iva-item-content-m2FiN");
+        findItem(driver, "[data-marker='catalog-serp']");
+
+        List<WebElement> printList = driver.findElements(By.cssSelector("[data-marker='item']"));
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Название принтера: " +
+                    printList.get(i).findElement(By.cssSelector("[itemprop='name']")).getText()
+                    + " Стоимость принтера: " + printList.get(i).findElement(By.cssSelector
+                    ("[data-marker='item-price']")).getText());
+        }
 
         driver.quit();
     }
